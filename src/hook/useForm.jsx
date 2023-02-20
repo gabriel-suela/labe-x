b6b6e50ebc892f098e6bcbba2d0912e2a1f2a4a7
gabriel-suela <gscsuela@gmail.com>
2023-02-20 12:33:35 -0300

    initial commit

---
+++ src/hook/useForm.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export const useForm = (InitialState) => {
+  const [form, setForm] = useState(InitialState);
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const clear = () => {
+    setForm(InitialState);
+  };
+
+  return [form, handleInput, clear];
+};
